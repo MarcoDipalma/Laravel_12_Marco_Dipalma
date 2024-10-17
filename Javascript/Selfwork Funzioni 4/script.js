6a8a1ccb15fff26c769e293298311ae01ae3d0be
Marco Dipalma <dipalmamarco14@gmail.com>
2024-10-17 11:56:14 +0200

    svolto selfwork funzioni 4

--- Javascript/Selfwork Funzioni 4/script.js
+++ Javascript/Selfwork Funzioni 4/script.js
@@ -1,13 +1,36 @@
+
+// let stringa = prompt('Digita ciò che vuoi, ti dirò se è un palindromo o no');
+
+// let stringa_senza_spazi = stringa.replace(/\W/g, "");
+
+// let stringa_capovolta = stringa_senza_spazi.split('').reverse().join('');
+
+// if (stringa_senza_spazi==stringa_capovolta) {
+//     console.log('Questa stringa è un palindromo');
+    
+// } else {
+//     console.log('Questa stringa non è un palindromo');
+
+// }
+
+
+function stringaPalindroma(str){
+    
+    let str_senza_spazi = str.replace(/\W/g, "");
+
+    let str_capovolta = str_senza_spazi.split('').reverse().join('');
+
+    if(str_senza_spazi==str_capovolta){
+        return true
+    } else{
+        return false
+    }
+}
+
 let stringa = prompt('Digita ciò che vuoi, ti dirò se è un palindromo o no');
 
-let stringa_senza_spazi = stringa.replace(/\W/g, "");
+console.log(stringaPalindroma(stringa));
 
-let stringa_capovolta = stringa_senza_spazi.split('').reverse().join('');
 
-if (stringa_senza_spazi==stringa_capovolta) {
-    console.log('Questa stringa è un palindromo');
-    
-} else {
-    console.log('Questa stringa non è un palindromo');
 
-}
+
