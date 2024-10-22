a0b585d40ed235449fc7e5e0c082377aaa1689e2
Marco Dipalma <dipalmamarco14@gmail.com>
2024-10-22 21:05:19 +0200

    svolto selfwork oggetti 1

---
+++ Javascript/Selfwork Oggetti 1/script.js
@@ -0,0 +1,10 @@
+let persona = {
+    nome : 'Marco',
+    cognome : 'Dipalma',
+    età : 21,
+    saluta : function() {
+        console.log('Ciao, io sono ' + this.nome); 
+    }
+}
+
+persona.saluta();
