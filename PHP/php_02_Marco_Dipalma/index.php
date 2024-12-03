3656bdf5629157a10eca4de92ab6d0c33390ddea
Marco Dipalma <dipalmamarco14@gmail.com>
2024-12-03 13:49:50 +0100

    svolto selfwork php 2

---
+++ PHP/php_02_Marco_Dipalma/index.php
@@ -0,0 +1,34 @@
+<?php
+
+// $
+
+// Variabili di partenza
+
+// $1text = "Marco";
+// $text2 = "hai";
+// $text.3 = "sete";
+// $text4 = "?";
+// @text5 = "Perchè";
+// $te-xt6 = '$text2';
+// $text 7 = 'bevuto';
+// $text8 = "tutto"
+
+// Stringa finale: “Marco Hai sete? Perche' hai bevuto tutto.“
+
+$text1 = "Marco ";
+$text2 = "hai ";
+$text3 = "sete";
+$text4 = "? ";
+$text5 = "Perchè ";
+$text6 = '$text2';
+$text7 = "bevuto ";
+$text8 = "tutto.";
+
+
+echo $text1 . $text2 . $text3 . $text4 . $text5 . $text2 . $text7 . $text8
+
+
+
+
+
+?>
