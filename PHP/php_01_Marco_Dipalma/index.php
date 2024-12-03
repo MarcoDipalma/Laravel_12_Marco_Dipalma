984cf7b6f47134d63389cda95077f2ad3bea9d58
Marco Dipalma <dipalmamarco14@gmail.com>
2024-12-03 13:26:38 +0100

    svolto traccia 1

---
+++ PHP/php_01_Marco_Dipalma/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// $
+
+// $int = 33;
+// $float = 12.3;
+// $string = 'Ciao';
+// $boolean = true;
+
+// var_dump($int);
+// var_dump($float);
+// var_dump($string);
+// var_dump($boolean);
+
+
+const INTERO = 33;
+const DECIMALE = 12.3;
+const STRINGA = 'Ciao';
+const BOOLEANO = true;
+
+var_dump(INTERO);
+var_dump(DECIMALE);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
+
+
+
+
+
+
+
+?>
