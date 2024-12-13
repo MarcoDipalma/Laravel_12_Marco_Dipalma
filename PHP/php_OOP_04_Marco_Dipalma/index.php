839e8f31aeab25e14036bf1639219a63a6033302
Marco Dipalma <dipalmamarco14@gmail.com>
2024-12-13 10:44:38 +0100

    svolto selfwork PHP OOP 4

---
+++ PHP/php_OOP_04_Marco_Dipalma/index.php
@@ -0,0 +1,85 @@
+<?php
+
+// $
+
+
+class Car {
+    private $num_telaio;
+
+    public function __construct($_num_telaio){
+        $this->num_telaio = $_num_telaio;
+    }
+
+    public function numeroTelaio(){
+        echo $this->num_telaio;
+    }
+
+}
+
+
+class Fiat extends Car {
+
+    protected $license;
+    protected $name;
+    protected $plate;
+
+    public function __construct($_num_telaio, $_name, $_plate){
+        
+        parent::__construct($_num_telaio);
+
+        $this->name = $_name;
+        $this->plate = $_plate;
+
+    }
+
+
+    public function descrizioneAuto(){
+
+        echo "La mia macchina è " . $this->name . " con targa " . $this->plate . " e numero di telaio ";
+        
+        Car::numeroTelaio();
+
+    }
+
+    
+}
+
+class Opel extends Car {
+
+    protected $license;
+    protected $name;
+    protected $plate;
+
+    public function __construct($_num_telaio, $_name, $_plate){
+        
+        parent::__construct($_num_telaio);
+
+        $this->name = $_name;
+        $this->plate = $_plate;
+
+    }
+
+
+    public function descrizioneAuto(){
+
+        echo "La mia macchina è " . $this->name . " con targa " . $this->plate . " e numero di telaio ";
+        
+        Car::numeroTelaio();
+
+    }
+
+    
+}
+
+
+$ciao = new Opel("1234", "Opel", "ND 123OJ");
+
+$ciao->descrizioneAuto();
+
+
+
+
+
+
+
+?>
