fd3274304bea01a04a382ef448500324171be895
Marco Dipalma <dipalmamarco14@gmail.com>
2025-01-09 21:18:33 +0100

    svolto selfwork Rotte e Viste

---
+++ Laravel/Laravel_01_Marco_Dipalma/resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="bg-dark">
+
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Homepage</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/pagina1">Pagina 1</a>
+        </li>
+        <li>
+          <a class="nav-link" href="/pagina2">Pagina 2</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+    <h1 class="text-white mt-3 d-flex justify-content-center">Homepage</h1>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
